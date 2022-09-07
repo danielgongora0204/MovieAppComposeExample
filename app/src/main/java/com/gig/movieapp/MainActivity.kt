32ede6dbf17d2d3aea8a48dcb3218033ce50e013
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-09-07 16:42:02 -0700

    Configured Gradle Dependencies
    Configured latest compose dependencies and deleted a deprecated mention.

--- app/src/main/java/com/gig/movieapp/MainActivity.kt
+++ app/src/main/java/com/gig/movieapp/MainActivity.kt
@@ -4,40 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.gig.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            App {
+                MainContent()
             }
         }
     }
 }
 
+@ExperimentalMaterial3Api
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App(content: @Composable () -> Unit) {
+    MovieAppTheme {
+//        Scaffold(
+//
+//        )
+    }
 }
 
+@Composable
+fun MainContent() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text(text = "Hello Android!")
+    }
+}
+
+@ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+    App {
+        MainContent()
     }
 }
