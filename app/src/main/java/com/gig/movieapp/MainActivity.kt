552a54b657180f5c01b7362d15b98ceeea0e73f3
danielgongora0204 <43617580+danielgongora0204@users.noreply.github.com>
2022-09-08 17:52:14 -0700

    Added Initial Scaffold
    Added initial scaffold to project.

--- app/src/main/java/com/gig/movieapp/MainActivity.kt
+++ app/src/main/java/com/gig/movieapp/MainActivity.kt
@@ -3,12 +3,17 @@ package com.gig.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gig.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             App {
-                MainContent()
+                MainContent(it)
             }
         }
     }
@@ -25,21 +30,33 @@ class MainActivity : ComponentActivity() {
 
 @ExperimentalMaterial3Api
 @Composable
-fun App(content: @Composable () -> Unit) {
+fun App(content: @Composable (Modifier) -> Unit) {
     MovieAppTheme {
-//        Scaffold(
-//
-//        )
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                            Text(text = "Hello from application")
+                    },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.LightGray),
+                )
+            }
+        ) {
+            content(
+                Modifier
+                    .padding(bottom = it.calculateBottomPadding(), top = it.calculateTopPadding()))
+        }
     }
 }
 
 @Composable
-fun MainContent() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Text(text = "Hello Android!")
+fun MainContent(modifier: Modifier, movieList: List<String> = listOf("Hello","There", "Person")) {
+    Column(modifier = modifier.padding(12.dp).fillMaxSize()) {
+        LazyColumn {
+            items(items = movieList) {
+                Text(text = it, color = Color.Black)
+            }
+        }
     }
 }
 
@@ -48,6 +65,6 @@ fun MainContent() {
 @Composable
 fun DefaultPreview() {
     App {
-        MainContent()
+        MainContent(it)
     }
 }
