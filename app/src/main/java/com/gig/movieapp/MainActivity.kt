e6fc4bba169ad86d3266e02349be42a4d5202a87
danielgongora0204 <arez.gig.xd.g@gmail.com>
2022-09-09 19:17:12 -0700

    Lazy Colum and Ktlint Implemented

--- app/src/main/java/com/gig/movieapp/MainActivity.kt
+++ app/src/main/java/com/gig/movieapp/MainActivity.kt
@@ -3,17 +3,37 @@ package com.gig.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.gig.movieapp.ui.theme.BackgroundLight
 import com.gig.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +56,7 @@ fun App(content: @Composable (Modifier) -> Unit) {
             topBar = {
                 TopAppBar(
                     title = {
-                            Text(text = "Hello from application")
+                        Text(text = "Hello from application")
                     },
                     colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.LightGray),
                 )
@@ -44,18 +64,62 @@ fun App(content: @Composable (Modifier) -> Unit) {
         ) {
             content(
                 Modifier
-                    .padding(bottom = it.calculateBottomPadding(), top = it.calculateTopPadding()))
+                    .padding(bottom = it.calculateBottomPadding(), top = it.calculateTopPadding())
+            )
         }
     }
 }
 
 @Composable
-fun MainContent(modifier: Modifier, movieList: List<String> = listOf("Hello","There", "Person")) {
-    Column(modifier = modifier.padding(12.dp).fillMaxSize()) {
+fun MainContent(
+    modifier: Modifier,
+    movieList: List<String> = listOf("Hello", "There", "Person")
+) {
+    Column(
+        modifier = modifier
+            .padding(12.dp)
+            .fillMaxSize()
+    ) {
         LazyColumn {
             items(items = movieList) {
-                Text(text = it, color = Color.Black)
+                MovieRow(movie = it)
             }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        colors = CardDefaults.cardColors(containerColor = BackgroundLight)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                color = BackgroundLight,
+                shape = RectangleShape,
+                tonalElevation = 4.dp,
+                shadowElevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image"
+                )
+            }
+
+            Text(text = movie, color = Color.Black)
         }
     }
 }
--- app/src/main/java/com/gig/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/gig/movieapp/ui/theme/Theme.kt
@@ -18,16 +18,21 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = BackgroundDark,
+    surface = SurfaceDark,
+    surfaceVariant = SurfaceDark
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    tertiary = Pink40,
+    background = BackgroundLight,
+    surface = SurfaceLight,
+    surfaceVariant = SurfaceLight
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
+    ,
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
